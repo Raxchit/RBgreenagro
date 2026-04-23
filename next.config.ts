d1f8ee13b0c4a58eaddc91ac13dac94a456fad25
RB Agro Admin <admin@rbagro.com>
2026-04-23 17:26:05 +0800

    Fix Vercel build error

--- next.config.ts
+++ next.config.ts
@@ -2,7 +2,9 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   output: "standalone",
-  /* config options here */
+  experimental: {
+    serverComponentsExternalPackages: ['@prisma/client', 'bcryptjs']
+  },
   typescript: {
     ignoreBuildErrors: true,
   },
